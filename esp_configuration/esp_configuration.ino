569eae1b03663c08ee1a06fbddf95916b7449635
AlexMLapusan <alex.m.lapusan@gmail.com>
2020-12-08 19:03:46 +0200

    Initial commit

---
+++ esp_configuration/esp_configuration.ino
@@ -0,0 +1,15 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h> 
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
